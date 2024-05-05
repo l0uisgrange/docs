173262d35030dd25f07816ff1b2c512e4f2afa73
Louis Grange <70532216+l0uisgrange@users.noreply.github.com>
2024-05-05 21:07:25 +0200

    New coming soon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next"
 import {Plus_Jakarta_Sans} from "next/font/google"
-import "../globals.css"
+import "./globals.css"
 import Icon from "@/components/icons"
 
 const inter = Plus_Jakarta_Sans({ subsets: ["latin"] })

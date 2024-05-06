9618b2a5e2f2a95083ee7f88e6e8bf584bda37b2
Louis Grange <70532216+l0uisgrange@users.noreply.github.com>
2024-05-06 09:49:18 +0200

    New homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next"
 import {Plus_Jakarta_Sans} from "next/font/google"
 import "./globals.css"
 import Icon from "@/components/icons"
+import {SiGithub, SiNpm} from '@icons-pack/react-simple-icons'
+import Link from "next/link";
 
 const inter = Plus_Jakarta_Sans({ subsets: ["latin"] })
 
@@ -15,7 +17,34 @@ export default function RootLayout({children,}: Readonly<{
 }>) {
     return (
         <html lang="en">
-            <body className={`${inter.className} flex items-top text-sm`}>
+            <body className={`${inter.className} dark:bg-black dark:text-white`}>
+                <div className={"border-b dark:border-neutral-800"}>
+                    <div className={"hidden max-w-7xl mx-auto gap-4 px-5 py-5 md:flex items-center"}>
+                        <span className={"flex-auto"}>Protocol.js</span>
+                        <a href={"https://github.com/protocoljs/"} className={"hover:opacity-75 transition"}>
+                            <SiGithub size={25}/>
+                        </a>
+                        <a href={"https://www.npmjs.com/package/@protocoljs/imap"}
+                           className={"hover:opacity-75 transition"}>
+                            <SiNpm size={25}/>
+                        </a>
+                        <button className={"flex items-center"}>
+                            <Icon tag={"drag_handle"}/>
+                        </button>
+                    </div>
+                </div>
+                <div className={"md:hidden border-b gap-4 px-5 py-5 dark:border-neutral-800 flex items-center"}>
+                    <span className={"flex-auto"}>Protocol.js</span>
+                    <a href={"https://github.com/protocoljs/"} className={"hover:opacity-75 transition"}>
+                        <SiGithub size={25}/>
+                    </a>
+                    <a href={"https://www.npmjs.com/package/@protocoljs/imap"} className={"hover:opacity-75 transition"}>
+                        <SiNpm size={25}/>
+                    </a>
+                    <button>
+                        <Icon tag={"drag_handle"} />
+                    </button>
+                </div>
                 <div className={"flex-auto min-h-screen"}>
                     {children}
                 </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     return (
         <main className="p-10 max-w-7xl mx-auto flex items-center h-screen">
             <div className={"text-center mx-auto"}>
-                <h1 className={"text-5xl lg:text-7xl font-bold tracking-tight text-gray-900 max-w-4xl mx-auto"}>Fast and lightweight mailbox for Node.js</h1>
-                <p className={"text-gray-700 text-5xl mt-5 mx-auto"}>Coming soon</p>
+                <h1 className={"text-5xl lg:text-8xl font-black tracking-tight text-neutral-900 dark:text-neutral-200 max-w-5xl mx-auto"}>Create your own mail server</h1>
+                <p className={"text-neutral-700 text-xl mt-20 mx-auto dark:text-zinc-500"}>Protocol.js allows you to create a <span className={"text-black dark:text-white font-medium"}>high-quality mail server</span> with Typescript</p>
             </div>
         </main>
     )
